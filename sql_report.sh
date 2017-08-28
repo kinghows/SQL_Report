6883deb59241ceb06e63c0cba043e83a80131e5f
kinghow <kinghow@hotmail.com>
2017-08-28 17:32:09 +0800

    send mail
    send mail

--- sql_report.sh
+++ sql_report.sh
@@ -3,4 +3,4 @@ report_base=/services/script
 datetime=`date +%Y-%m-%d_%H-%M`
 python $report_base/sql_report.py -p $report_base/dbset101.ini  -s html>$report_base/my_report101_$datetime.html
 python $report_base/sql_report.py -p $report_base/dbset102.ini  -s html>$report_base/my_report102_$datetime.html
-python SendEmail.py -p emailset101.ini -f $report_base/my_report101_$datetime.html,$report_base/my_report102_$datetime.html
+python $report_base/SendEmail.py -p $report_base/emailset101.ini -f $report_base/my_report101_$datetime.html,$report_base/my_report102_$datetime.html
